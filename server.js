392a4ad43e3d6e577a4c4c0ccbeae37214b32b44
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-05-27 06:05:13 +0000

    first group test

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 var fs = require('fs');
 
-function handler(req, res) {
+var groups = {};
+
+var app = require('http').createServer(function(req, res) {
     fs.readFile('index.html', function(err, data) {
         if (err) {
             res.writeHead(500);
             return res.end('Error loading index.html');
         }
-
+        groups[req.url] = io.of(req.url).on('connection', function(socket) {
+            socket.on('message', function(data) {
+                console.info(data);
+                socket.send("[ECHO] " + data);
+                groups[req.url].emit("[ECHO] " + data);
+            });
+            /*group[req.url].emit('a message', {
+                everyone: 'in',
+                '/chat': 'will get'
+            });
+            socket.emit('a message', {
+                that: 'only',
+                '/chat': 'will get'
+            });*/
+        });
         res.writeHead(200, {
             'Content-Type': 'text/html',
             "Content-Length": data.length
         });
         res.end(data);
     });
-}
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app);
-
+});
 app.listen(process.env.PORT || process.env.C9_PORT || 8001);
 
-io.sockets.on('connection', function(socket) {
-    // echo the message
+var io = require('socket.io').listen(app);
+/*io.sockets.on('connection', function(socket) {
+    console.info("Group::: " + io.of());
     socket.on('message', function(data) {
         console.info(data);
         socket.send("[ECHO] " + data);
     });
-});
+});*/

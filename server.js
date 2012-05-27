653738e0689767d904d553e47167d80df9dc4ebc
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-05-27 18:24:06 +0000

    Updated Interface and some other minor improvements

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 var fs = require('fs');
-
 var groups = {};
-
 var app = require('http').createServer(function(req, res) {
-    fs.readFile('index.html', function(err, data) {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
-        var group=req.url;
-        if(!groups[group])groups[group] = io.of(group).on('connection', function(socket) {
-            socket.on('message', function(data) {
-                console.info(data);
-                console.info("Name: " + data.name);
-                console.info("Msg: " + data.msg);
-                groups[group].emit("message", {
-                    name: data.name,
-                    msg: data.msg
-                });
+    var group = req.url;
+    if ((group.length>1)&&(!groups[group])) groups[group] = io.of(group).on('connection', function(socket) {
+        socket.on('message', function(data) {
+            console.info(data);
+            console.info("Name: " + data.name);
+            console.info("Msg: " + data.msg);
+            groups[group].emit("message", {
+                name: data.name,
+                msg: data.msg
             });
         });
+    });
+    fs.readFile((group.length>1)?'chat.html':'index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading page!');
+        }
         res.writeHead(200, {
             'Content-Type': 'text/html',
             "Content-Length": data.length
@@ -28,12 +26,4 @@ var app = require('http').createServer(function(req, res) {
     });
 });
 app.listen(process.env.PORT || process.env.C9_PORT || 8001);
-
 var io = require('socket.io').listen(app);
-/*io.sockets.on('connection', function(socket) {
-    console.info("Group::: " + io.of());
-    socket.on('message', function(data) {
-        console.info(data);
-        socket.send("[ECHO] " + data);
-    });
-});*/

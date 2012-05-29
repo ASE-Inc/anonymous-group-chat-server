d7146e9aadad6b261edad675335b158ca27a73aa
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-05-29 04:14:06 +0000

    Distributed chat network test 2

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 var fs = require('fs'),
     ioClient = require('socket.io-client'),
     groups = {},
-    distributionServers = ["http://agcmd1.herokuapp.com/", "http://agcmd2.herokuapp.com/", "http://agcmd3.herokuapp.com/"],
+    distributionServers = ["http://agcmd1.herokuapp.com", "http://agcmd2.herokuapp.com", "http://agcmd3.herokuapp.com"],
     distributionSockets = [],
-    chatServers = ["http://agc1.abhishekmunie.com", "http://agc2.abhishekmunie.com", "http://agc3.abhishekmunie.com"];
+    chatServers = ["http://agc.abhishekmunie.com", "http://agc1.abhishekmunie.com", "http://agc2.abhishekmunie.com", "http://agc3.abhishekmunie.com"];
 var distributionLen = distributionServers.length;
-for (var len = distributionLen; len;) {
-    var ds = new ioClient.Socket(distributionServers[--len]);
-    ds.connect();
-    ds.on('createGroup', function(group) {
-        createGroup(group);
-    });
-    distributionSockets[len] = ds;
+
+function generateDistributionSockets(group) {
+    var distributionSockets = [];
+    for (var len = distributionLen; len;) {
+        var ds = new ioClient.Socket(distributionServers[--len]);
+        ds.connect();
+        ds.on('createGroup', function(group) {
+            createGroup(group);
+        });
+        distributionSockets[len] = ds;
+    }
 }
+distributionSockets = generateDistributionSockets("");
 
 function distributeMessage(sockets, data) {
-    sockets.emit("message", data);
+    sockets[Math.floor(Math.random() * distributionLen)].emit("message", data);
 }
 
 function createGroup(group) {
     if ((group.length == 1) || groups[group]) return;
     groups[group] = {
-        user: {},
+        users: {},
+        distributionSockets: generateDistributionSockets(group),
         socket: io.of(group).on('connection', function(socket) {
+            var user = {
+                name: undefined,
+                socket: socket
+            };
             socket.on('message', function(data) {
                 distributeMessage(groups[group], data);
             });
             socket.on('disconnect', function() {});
             socket.on('setName', function(name) {
                 socket.set('name', name, function() {
-                    socket.emit('ready');
+                    user.name = name;
+                    groups[group].users[name] = user;
+                    //socket.emit('ready');
                 });
             });
             socket.on('msg', function() {

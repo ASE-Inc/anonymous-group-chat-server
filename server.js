e273c47293cdefe2240db0ab4f06d8d50ff32d6e
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-06-02 11:52:16 +0000

    added support for custom 404 and removed extra code

--- server.js
+++ server.js
@@ -65,44 +65,26 @@ var app = require('h5bp').server(require('express'), {
     root: __dirname + "/",
     maxAge: 1000 * 60 * 60
 });
-/*var app = require('http').createServer(function(req, res) {
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-    path.exists(filename, function(exists) {
-        if (!exists) {
-            res.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            filename = path.join(process.cwd(), url.parse('/404.html').pathname);
-        }
-        else {
-            res.writeHead(200, {
-                'Content-Type': mimeTypes[path.extname(filename).split(".")[1]]
-            });
-        }
-        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-        var ua = req.headers['user-agent'];
-        if (ua && ua.indexOf('MSIE') > -1 && /html?($|\?|#)/.test(req.url)) {
-            res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
-        }
-        var fileStream = fs.createReadStream(filename);
-        fileStream.pipe(res);
+app.get('*', function(req, res) {
+    res.writeHead(404, {
+        "Content-Type": "text/html"
     });
-});*/
+    fs.createReadStream(path.join(process.cwd(), url.parse('/404.html').pathname)).pipe(res);
+});
 var io = require('socket.io').listen(app);
 app.listen(process.env.C9_PORT || process.env.PORT || process.env.VCAP_APP_PORT || process.env.VMC_APP_PORT || 1337 || 8001);
-/* io.configure(function() {
-    //Production Settings
+io.configure(function() {
+/* //Production Settings
     io.enable('browser client minification');
     io.enable('browser client etag');
     io.enable('browser client gzip');
     io.set('log level', 1);
-    io.set('transports', ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'flashsocket']);
+    io.set('transports', ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'flashsocket']);*/
     io.set("polling duration", 10);
     io.set('authorization', function(handshakeData, callback) {
         callback(null, true); // error first callback style 
     });
-});*/
+});
 io.sockets.on('connection', function(socket) {
     var user = {
         name: undefined,
@@ -132,170 +114,3 @@ io.sockets.on('connection', function(socket) {
     });
 });
 generateGroup("/");
-var mimeTypes = {
-    ".3gp": "video/3gpp",
-    ".a": "application/octet-stream",
-    ".ai": "application/postscript",
-    ".aif": "audio/x-aiff",
-    ".aiff": "audio/x-aiff",
-    ".asc": "application/pgp-signature",
-    ".asf": "video/x-ms-asf",
-    ".asm": "text/x-asm",
-    ".asx": "video/x-ms-asf",
-    ".atom": "application/atom+xml",
-    ".au": "audio/basic",
-    ".avi": "video/x-msvideo",
-    ".bat": "application/x-msdownload",
-    ".bin": "application/octet-stream",
-    ".bmp": "image/bmp",
-    ".bz2": "application/x-bzip2",
-    ".c": "text/x-c",
-    ".cab": "application/vnd.ms-cab-compressed",
-    ".cc": "text/x-c",
-    ".chm": "application/vnd.ms-htmlhelp",
-    ".class": "application/octet-stream",
-    ".com": "application/x-msdownload",
-    ".conf": "text/plain",
-    ".cpp": "text/x-c",
-    ".crt": "application/x-x509-ca-cert",
-    ".css": "text/css",
-    ".csv": "text/csv",
-    ".cxx": "text/x-c",
-    ".deb": "application/x-debian-package",
-    ".der": "application/x-x509-ca-cert",
-    ".diff": "text/x-diff",
-    ".djv": "image/vnd.djvu",
-    ".djvu": "image/vnd.djvu",
-    ".dll": "application/x-msdownload",
-    ".dmg": "application/octet-stream",
-    ".doc": "application/msword",
-    ".dot": "application/msword",
-    ".dtd": "application/xml-dtd",
-    ".dvi": "application/x-dvi",
-    ".ear": "application/java-archive",
-    ".eml": "message/rfc822",
-    ".eps": "application/postscript",
-    ".exe": "application/x-msdownload",
-    ".f": "text/x-fortran",
-    ".f77": "text/x-fortran",
-    ".f90": "text/x-fortran",
-    ".flv": "video/x-flv",
-    ".for": "text/x-fortran",
-    ".gem": "application/octet-stream",
-    ".gemspec": "text/x-script.ruby",
-    ".gif": "image/gif",
-    ".gz": "application/x-gzip",
-    ".h": "text/x-c",
-    ".hh": "text/x-c",
-    ".htm": "text/html",
-    ".html": "text/html",
-    ".ico": "image/vnd.microsoft.icon",
-    ".ics": "text/calendar",
-    ".ifb": "text/calendar",
-    ".iso": "application/octet-stream",
-    ".jar": "application/java-archive",
-    ".java": "text/x-java-source",
-    ".jnlp": "application/x-java-jnlp-file",
-    ".jpeg": "image/jpeg",
-    ".jpg": "image/jpeg",
-    ".js": "application/javascript",
-    ".json": "application/json",
-    ".log": "text/plain",
-    ".m3u": "audio/x-mpegurl",
-    ".m4v": "video/mp4",
-    ".man": "text/troff",
-    ".mathml": "application/mathml+xml",
-    ".mbox": "application/mbox",
-    ".mdoc": "text/troff",
-    ".me": "text/troff",
-    ".mid": "audio/midi",
-    ".midi": "audio/midi",
-    ".mime": "message/rfc822",
-    ".mml": "application/mathml+xml",
-    ".mng": "video/x-mng",
-    ".mov": "video/quicktime",
-    ".mp3": "audio/mpeg",
-    ".mp4": "video/mp4",
-    ".mp4v": "video/mp4",
-    ".mpeg": "video/mpeg",
-    ".mpg": "video/mpeg",
-    ".ms": "text/troff",
-    ".msi": "application/x-msdownload",
-    ".odp": "application/vnd.oasis.opendocument.presentation",
-    ".ods": "application/vnd.oasis.opendocument.spreadsheet",
-    ".odt": "application/vnd.oasis.opendocument.text",
-    ".ogg": "application/ogg",
-    ".p": "text/x-pascal",
-    ".pas": "text/x-pascal",
-    ".pbm": "image/x-portable-bitmap",
-    ".pdf": "application/pdf",
-    ".pem": "application/x-x509-ca-cert",
-    ".pgm": "image/x-portable-graymap",
-    ".pgp": "application/pgp-encrypted",
-    ".pkg": "application/octet-stream",
-    ".pl": "text/x-script.perl",
-    ".pm": "text/x-script.perl-module",
-    ".png": "image/png",
-    ".pnm": "image/x-portable-anymap",
-    ".ppm": "image/x-portable-pixmap",
-    ".pps": "application/vnd.ms-powerpoint",
-    ".ppt": "application/vnd.ms-powerpoint",
-    ".ps": "application/postscript",
-    ".psd": "image/vnd.adobe.photoshop",
-    ".py": "text/x-script.python",
-    ".qt": "video/quicktime",
-    ".ra": "audio/x-pn-realaudio",
-    ".rake": "text/x-script.ruby",
-    ".ram": "audio/x-pn-realaudio",
-    ".rar": "application/x-rar-compressed",
-    ".rb": "text/x-script.ruby",
-    ".rdf": "application/rdf+xml",
-    ".roff": "text/troff",
-    ".rpm": "application/x-redhat-package-manager",
-    ".rss": "application/rss+xml",
-    ".rtf": "application/rtf",
-    ".ru": "text/x-script.ruby",
-    ".s": "text/x-asm",
-    ".sgm": "text/sgml",
-    ".sgml": "text/sgml",
-    ".sh": "application/x-sh",
-    ".sig": "application/pgp-signature",
-    ".snd": "audio/basic",
-    ".so": "application/octet-stream",
-    ".svg": "image/svg+xml",
-    ".svgz": "image/svg+xml",
-    ".swf": "application/x-shockwave-flash",
-    ".t": "text/troff",
-    ".tar": "application/x-tar",
-    ".tbz": "application/x-bzip-compressed-tar",
-    ".tcl": "application/x-tcl",
-    ".tex": "application/x-tex",
-    ".texi": "application/x-texinfo",
-    ".texinfo": "application/x-texinfo",
-    ".text": "text/plain",
-    ".tif": "image/tiff",
-    ".tiff": "image/tiff",
-    ".torrent": "application/x-bittorrent",
-    ".tr": "text/troff",
-    ".txt": "text/plain",
-    ".vcf": "text/x-vcard",
-    ".vcs": "text/x-vcalendar",
-    ".vrml": "model/vrml",
-    ".war": "application/java-archive",
-    ".wav": "audio/x-wav",
-    ".wma": "audio/x-ms-wma",
-    ".wmv": "video/x-ms-wmv",
-    ".wmx": "video/x-ms-wmx",
-    ".wrl": "model/vrml",
-    ".wsdl": "application/wsdl+xml",
-    ".xbm": "image/x-xbitmap",
-    ".xhtml": "application/xhtml+xml",
-    ".xls": "application/vnd.ms-excel",
-    ".xml": "application/xml",
-    ".xpm": "image/x-xpixmap",
-    ".xsl": "application/xml",
-    ".xslt": "application/xslt+xml",
-    ".yaml": "text/yaml",
-    ".yml": "text/yaml",
-    ".zip": "application/zip"
-};
